a87fd138ebbc3db42c60fe6f6c97d88484297f4b
sudhir600 <sudhirgupta.456@gmail.com>
2019-05-15 17:57:17 +0530

    endpoint contract.js added

---
+++ src/endpoints/contract.js
@@ -0,0 +1,73 @@
+const {buildUrl} = require('../utils')
+
+module.exports = {
+
+	/**
+	 * @param page
+	 * @param pageSize
+	 * @param sort
+	 * @returns {Promise<*|Promise<*>|Promise|Promise<Response>|never>}
+	 */
+	async getContracts(fetch, {page, pageSize, sort}) {
+		return fetch(buildUrl('contract', {page, pageSize, sort}))
+	},
+
+	/**
+	 * @param body
+	 * @returns {Promise<*|Promise<*>|Promise|Promise<Response>|never>}
+	 */
+	async createContract(fetch, body) {
+		return fetch('contract', {method: 'POST', body})
+	},
+
+	/**
+	 * @returns {Promise<*|Promise<*>|Promise|Promise<Response>|never>}
+	 */
+	async getContactCount(fetch) {
+		return fetch('contract/count')
+	},
+
+	/**
+	 * @param id
+	 * @returns {Promise<*|Promise<*>|Promise|Promise<Response>|never>}
+	 */
+	async deleteContractById(fetch, id) {
+		return fetch(buildUrl(`contract/id/${id}`, {id}), {method: 'DELETE'})
+	},
+
+	/**
+	 * @param id
+	 * @returns {Promise<*|Promise<*>|Promise|Promise<Response>|never>}
+	 */
+	async getContractById(fetch, id) {
+		return fetch(buildUrl(`contract/id/${id}`, {id}))
+	},
+
+	/**
+	 * @param id
+	 * @param body
+	 * @returns {Promise<*|Promise<*>|Promise|Promise<Response>|never>}
+	 */
+	async updateContractForId(fetch, id, body) {
+		return fetch(buildUrl(`contract/id/${id}`, {id}), {method: 'PUT', body})
+	},
+
+	/**
+	 * @param id
+	 * @param scaleWidth
+	 * @param scaleHeight
+	 * @returns {Promise<*|Promise<*>|Promise|Promise<Response>|never>}
+	 */
+	async getContractImageById(fetch, {id, scaleWidth, scaleHeight}) {
+		return fetch(buildUrl(`contract/id/${id}/downloadImage`, {id, scaleWidth, scaleHeight}))
+	},
+
+	/**
+	 * @param id
+	 * @param body
+	 * @returns {Promise<*|Promise<*>|Promise|Promise<Response>|never>}
+	 */
+	async createContractImageForId(fetch, id, body) {
+		return fetch(buildUrl(`contract/id/${id}/uploadImage`, {id}), {method: 'POST', body})
+	}
+}
